33b73c8137083f48f03039106c98f6bb237f97e5
IvanHeriver <ihach_ihach@hotmail.com>
2021-06-08 08:52:33 +0200

    interesting event handler to ask user before closing tab (kept here for reference)

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ const app = new App({
 	}
 });
 
+console.log("HELLO")
 // window.console.log = () => {}
+// window.addEventListener('beforeunload', function (e) {
+// 	// Cancel the event
+// 	e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+// 	// Chrome requires returnValue to be set
+// 	e.returnValue = '';
+//   });
 
 export default app;
