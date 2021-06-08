19f89e4dcd812e1d7e9b47f1d0dadd91e2687a14
IvanHeriver <ihach_ihach@hotmail.com>
2021-06-08 08:53:36 +0200

    some code for debugging purposes

--- src/logic/constants.js
+++ src/logic/constants.js
@@ -1,6 +1,5 @@
 export const LETTERS = {
     _: {pts: 0, n: 2, vowel: true, consonant: true},
-    // _: {pts: 0, n: 10, vowel: true, consonant: true},
     A: {pts: 1, n: 9, vowel: true, consonant: false},
     B: {pts: 3, n: 2, vowel: false, consonant: true},
     C: {pts: 3, n: 2, vowel: false, consonant: true},
@@ -28,6 +27,35 @@ export const LETTERS = {
     Y: {pts: 10, n: 1, vowel: true, consonant: false},
     Z: {pts: 10, n: 1, vowel: false, consonant: true},
 }
+// export const LETTERS = {
+//     _: {pts: 0, n: 0, vowel: true, consonant: true},
+//     A: {pts: 1, n: 900, vowel: true, consonant: false},
+//     B: {pts: 3, n: 0, vowel: false, consonant: true},
+//     C: {pts: 3, n: 0, vowel: false, consonant: true},
+//     D: {pts: 2, n:0, vowel: false, consonant: true},
+//     E: {pts: 1, n: 0, vowel: true, consonant: false},
+//     F: {pts: 4, n: 0, vowel: false, consonant: true},
+//     G: {pts: 2, n: 0, vowel: false, consonant: true},
+//     H: {pts: 4, n: 0, vowel: false, consonant: true},
+//     I: {pts: 1, n: 0, vowel: true, consonant: false},
+//     J: {pts: 8, n: 0, vowel: false, consonant: true},
+//     K: {pts: 10, n: 0, vowel: false, consonant: true},
+//     L: {pts: 1, n: 0, vowel: false, consonant: true},
+//     M: {pts: 2, n: 0, vowel: false, consonant: true},
+//     N: {pts: 1, n: 0, vowel: false, consonant: true},
+//     O: {pts: 1, n: 0, vowel: true, consonant: false},
+//     P: {pts: 3, n: 0, vowel: false, consonant: true},
+//     Q: {pts: 8, n:0, vowel: false, consonant: true},
+//     R: {pts: 1, n: 0, vowel: false, consonant: true},
+//     S: {pts: 1, n: 0, vowel: false, consonant: true},
+//     T: {pts: 1, n: 0, vowel: false, consonant: true},
+//     U: {pts: 1, n: 0, vowel: true, consonant: false},
+//     V: {pts: 4, n: 0, vowel: false, consonant: true},
+//     W: {pts: 10, n: 0, vowel: false, consonant: true},
+//     X: {pts: 10, n:0, vowel: false, consonant: true},
+//     Y: {pts: 10, n: 1000, vowel: true, consonant: false},
+//     Z: {pts: 10, n: 0, vowel: false, consonant: true},
+// }
 let ltrs = Object.keys(LETTERS).filter(e=>e!=="_")
 export const LTRS = ltrs
 
@@ -38,6 +66,13 @@ export const CELLS = {
     word_double: [16, 28, 32, 42, 48, 56, 64, 70, 112, 154, 160, 168, 176, 182, 192, 196, 208],
     word_triple: [0, 7, 14, 105, 119, 210, 217, 224]
 }
+// export const CELLS = {
+//     letter_double: [3, 11, 36, 38, 45, 52, 59, 92, 96, 98, 102, 108,
+//          116, 122,  128, 132, 165, 172, 179, 186, 188, 213, 221],
+//     letter_triple: [20, 24, 76, 80, 84, 88, 136, 140, 144, 148, 200, 204],
+//     word_double: [16, 28, 32, 42, 48, 56, 64, 70, 112, 154, 160, 168, 176, 182, 192, 196, 208, 126],
+//     word_triple: [0, 7, 14, 105, 119, 210, 217, 224]
+// }
 
 export const POINTS = Array(15 * 15).fill(0).map((e, i) => {
     const ld = CELLS.letter_double.indexOf(i)===-1 ? 1 : 2
