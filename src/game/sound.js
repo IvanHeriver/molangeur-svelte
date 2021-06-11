9a08caae940af224076f8707b28f70659ae96f14
IvanHeriver <ihach_ihach@hotmail.com>
2021-06-11 13:02:55 +0200

    try to have more reactive sounds

--- src/game/sound.js
+++ src/game/sound.js
@@ -4,12 +4,23 @@ const drop_board = new Tone.Player("../images/drop_board.wav").toDestination()
 const drop_rack = new Tone.Player("../images/drop_rack.wav").toDestination()
 drop_board.volume.value = -15
 drop_rack.volume.value = -25
-Tone.loaded()
+Tone.setContext(new Tone.Context({ latencyHint : "playback" }))
+
+const init = () => {
+    Tone.loaded()
+    document.removeEventListener("mouseover", init)
+    document.removeEventListener("scroll", init)
+    document.removeEventListener("keydown", init)
+}
+document.addEventListener("mouseover", init)
+document.addEventListener("scroll", init)
+document.addEventListener("keydown", init)
 
 export const soundDropBoard = () => {
-    drop_board.start()
+    // drop_board.start("0")
+    drop_board.start("-0.1")
 }
 
 export const soundDropRack = () => {
-    drop_rack.start()
+    drop_rack.start("-0.1")
 }
