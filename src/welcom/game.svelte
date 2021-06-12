8c566105bc4ca78b0396d4d652b9c8e9fa9c1a86
IvanHeriver <ihach_ihach@hotmail.com>
2021-06-12 09:38:22 +0200

    added information on game cards

--- src/welcom/game.svelte
+++ src/welcom/game.svelte
@@ -20,8 +20,8 @@
 
 <div class="game" on:click={()=>onclick(game.id)}>
     <div class="title">
-        <div class="main">Partie créée le <span class="date">{getDate(game.create_date)}</span>
-           à <span class="date">{getTime(game.create_date)}</span></div>
+        <div class="main">Partie créée le <span class="bold">{getDate(game.create_date)}</span>
+           à <span class="bold">{getTime(game.create_date)}</span></div>
         <div class="secondary">
            <!-- <span class="id">
                {`(id #${game.id})`}
@@ -31,8 +31,12 @@
            </button>
         </div>
     </div>
-    <div>Dernière modification le <span class="date">{getDate(game.update_date)}</span>
-        à <span class="date">{getTime(game.update_date)}</span></div>
+    <div>Dernière modification le <span class="bold">{getDate(game.update_date)}</span>
+        à <span class="bold">{getTime(game.update_date)}</span></div>
+    <div>Score: <span class="bold">{game.players[0].score}</span></div>
+    <div>Score de Maître MoLangeur: <span class="bold">{game.players[0].molangeur}</span></div>
+    <!-- <div>Différence de score: <span class="bold">{game.players[0].molangeur - game.players[0].score}</span></div> -->
+    <!-- <div>Nombre de tours faits: <span class="bold">{game.round}</span></div> -->
     <!-- <div class="content"> -->
     <!-- <img src={game.img} alt="" width="200px" height="200px"> -->
     <div class="img">
@@ -79,17 +83,17 @@
         align-items: center;
         border-bottom: 1px solid grey;
     }
-    .date {
+    /* .date {
         font-weight: bold;
-    }
+    } */
     /* .content {
         display: flex;
         justify-content: flex-start;
         font-size: 0.9em;
         padding-top: 0.5em;
-    }
+    } */
 
     .bold {
         font-weight: bold;
-    } */
+    }
 </style>
